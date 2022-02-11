080b95d5be979421928f5fe176fb849b4f126de8
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-02-11 18:33:03 -0500

    gem collision test case is added

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -6,23 +6,31 @@ using UnityEngine.TestTools;
 
 public class NewTestScript
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void NewTestScriptSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
+    
+    // This test is for verifying that Gem is destroyed when it collides.
     [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    public IEnumerator TestForGemCollision()
     {
         GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerPaddle");
+        GameObject gemPrefab = Resources.Load<GameObject>("Prefabs/Gem");
         GameObject paddle;
+        GameObject gem;
         paddle = GameObject.Instantiate(playerPrefab);
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        gem = GameObject.Instantiate(gemPrefab);
+        
+        Assert.IsNotNull(playerPrefab);
+        Assert.IsNotNull(gem);
         yield return null;
+
+        // translate gem to the paddle
+        gem.transform.position = paddle.transform.position;
+        
+        // wait for collision process
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+
+        // if true, gem was destroyed
+        Assert.IsTrue(gem == null);
+
     }
 }
--- Assets/[Scripts]/Gem.cs
+++ Assets/[Scripts]/Gem.cs
@@ -18,6 +18,7 @@ public class Gem : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Gem Destroyed!");
         Destroy(gameObject);
     }
 }
